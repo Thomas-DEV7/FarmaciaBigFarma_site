7866e3aa6583d1790b9a567ea43cc6da6192d2cb
Thomas-DEV7 <thomas.felip16@gmail.com>
2023-03-17 21:53:08 -0300

    Add variaveis das sessões para começar as renderizações condicionais.

--- js/index.js
+++ js/index.js
@@ -10,13 +10,21 @@ function menuShow(){
     }
 }
 // Renderizações condicionais por click
+    //Variaveis dos itens para mudar a renderização de produtos.
 const cg1 = document.querySelector("#cg1");
 const cg2 = document.querySelector("#cg2");
 const cg3 = document.querySelector("#cg3");
 const cg4 = document.querySelector("#cg4");
 const cg5 = document.querySelector("#cg5");
 const cg6 = document.querySelector("#cg6");
-
+    //Variaveis das sessões de produtos
+const cat1 = document.querySelector("#cat1");
+const cat2 = document.querySelector("#cat2");
+const cat3 = document.querySelector("#cat3");
+const cat4 = document.querySelector("#cat4");
+const cat5 = document.querySelector("#cat5");
+const cat6 = document.querySelector("#cat6");
+    
 cg1.addEventListener('click', function(){  //para testes
     alert("funcionando.1");
 });
